e4315d40f14234707df507c7e968a428479505bd
deepkashiwa20 <jiangrh@csis.u-tokyo.ac.jp>
2021-07-01 18:58:12 +0900

    model

--- model/Param.py
+++ model/Param.py
@@ -12,7 +12,7 @@ PATIENCE = 20
 LOSS = 'mae'
 OPTIMIZER = 'adam'
 TRAINRATIO = 0.8
-VALIDRATIO = 0.8 # 0.8 * 0.8 = 0.64 as training data and 0.16 as validation data
+VALIDRATIO = 0.8 # 0.8 * 0.8 = 0.64 as training data and 0.16 as validation data # Used for Torch.
 ODPATH = '../data/od_day20180101_20210228.npz'
 ADJPATH = '../data/adjacency_matrix.npy'
 OD_DAYS = [date.strftime('%Y-%m-%d') for date in pd.date_range(start='2018-01-01', end='2021-02-28', freq='1D')]

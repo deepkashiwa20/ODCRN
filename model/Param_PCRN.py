c54fdbec147e12b46c254c5049103a1cfe6abed8
deepkashiwa20 <54202779+deepkashiwa20@users.noreply.github.com>
2021-07-06 13:10:26 +0900

    Delete model directory

--- model/Param_PCRN.py
+++
@@ -1,2 +0,0 @@
-len_c, len_p, len_t = 7, 7, 7
-DAYINFO = True
--- model/Param_STResNet.py
+++
@@ -1,6 +0,0 @@
-nb_res_units = 2
-# nb_res_units = 12
-nb_filters = 32
-nb_channel = 1     # population density
-len_c, len_p, len_t = 7, 7, 7
-DAYINFO = True
